e56ce48ee531cf946388fe738300cd78237d9e70
saroj <saroj4150@gmail.com>
2025-12-28 19:45:20 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,10 @@
+def add2numbers(a,b):   #parameter (a,b)
+    result = a + b
+    print("The sum is: ", result)
+
+#call above sum function
+add2numbers(5,3)    #arguments (5,3)
+
+add2numbers(a=10, b=100)
+
+add2numbers(b=50, a=10)
